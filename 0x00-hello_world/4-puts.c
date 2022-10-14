81032edd7f9c80f1c21f6b8995b1b78f2c8fa6c2
Erickkaranja <karanja99erick@gmail.com>
2022-10-14 09:18:51 -0700

    use of puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *c program that prints 
- *programming is like
- *building multilingual puzzle
+ *Main : code entry
+ *
+ *Return 0 if successful
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
