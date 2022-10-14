3402a7c091ddc300a90dfbf9cc3f77cfb040a652
Erickkaranja <karanja99erick@gmail.com>
2022-10-14 07:05:09 -0700

    size of different data types

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *c program that prints 
+ *programming is like
+ *building multilingual puzzle
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main -start of code execution
+*
+*/
+int main(void)
+{
+printf("size of character is %d byte(s)\n", sizeof(char));
+printf("size of integer is %d byte(s)\n", sizeof(int));
+printf("size of float is %d byte(s)\n", sizeof(float));
+printf("size of double is %d byte(s)\n", sizeof(double));
+return (0);
+}
