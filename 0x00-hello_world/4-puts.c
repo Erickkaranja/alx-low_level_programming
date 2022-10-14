ecd503d5b2dc1703b1661c0f061fc5682ed2482c
Erickkaranja <karanja99erick@gmail.com>
2022-10-14 11:30:57 -0700

    task 8

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  *main -How to use puts function
- *puts(Programming is like building a multilingual puzzle)
- *return: Always 0 (success)
+ *puts(\"Programming is like building a multilingual puzzle)
+ *Return: Always 0 (success)
  */
 int main(void)
 {
