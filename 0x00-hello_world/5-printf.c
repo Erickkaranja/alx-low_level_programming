bc479b3f3b01644e2caf4d8ba340dacc24067d6c
Erickkaranja <karanja99erick@gmail.com>
2022-10-14 07:21:50 -0700

    prints script

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main use of printf
+ *main -Entry point
  *
- *returns 0 if successful
+ *Return: Always 0 (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art, \n");
 return (0);
 }
