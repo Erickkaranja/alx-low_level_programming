134bed00d21a5c7a35d29c2984edfd18bea13af6
Erickkaranja <karanja99erick@gmail.com>
2022-10-15 00:31:43 -0700

    practice

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main - prints lowercase letters in reverse
+ *Return Always 0
+ */
+int main(void)
+{
+	char re;
+	for(re= 'z';re >= 'a' ;re--)
+		putchar(re);
+	putchar('\n');
+	return (0);
+}
