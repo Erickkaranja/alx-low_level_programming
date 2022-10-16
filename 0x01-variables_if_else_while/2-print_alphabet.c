1798a4f7f03b5a3d74a5e39c71c51ccb56504bf9
Erickkaranja <karanja99erick@gmail.com>
2022-10-16 00:36:19 -0700

    printing alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,14 @@
  */
 int main(void)
 {
-	char ls:
-	for (ls='a';ls<='z':ls++);
+	char ls;
+
+	for (ls = 'a' ; ls <= 'z' ; ls++)
 	{
 	putchar(ls);
 	}
+
 	putchar('\n');
-	return(0);
+
+	return (0);
 }
