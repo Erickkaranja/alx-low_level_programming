7444c401955a240d3d01244d457eebb342205f72
Erickkaranja <karanja99erick@gmail.com>
2022-10-16 19:42:20 -0700

    single digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,7 +11,10 @@ int main(void)
 	for (n = 0 ; n <= 9 ; n++)
 	{
 		putchar((n % 10) + '0');
-		putchar(',');
+
+		if (n == 9)
+			continue;
+			putchar(',');
 			putchar(' ');
 	}
 	putchar('\n');
