585d74cbbc3a1d8d8f90653d760298293ac779b1
Erickkaranja <karanja99erick@gmail.com>
2022-10-18 12:58:10 -0700

    nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,12 +10,12 @@ int main(void)
 	int i = 0;
 	long j = 1, k = 2;
 
-	while (i <50)
+	while (i < 50)
 	{
 		if (i == 0)
-			printf("%ld", j);
+			printf("ld", j);
 		else if (i == 1)
-			printf(", %ld", k);
+			printf(", %ld",k);
 		else
 		{
 			k += j;
@@ -23,9 +23,9 @@ int main(void)
 			printf(", %ld", k);
 		}
 
-		i++;
+		++i;
 	}
-
 	printf("\n");
+
 	return (0);
 }
