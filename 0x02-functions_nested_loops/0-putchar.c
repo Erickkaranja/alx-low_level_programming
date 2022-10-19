e55c399717b44ba3b5732b6445d733f2ff353fb7
Erickkaranja <karanja99erick@gmail.com>
2022-10-18 19:53:06 -0700

    printing characters

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,16 +3,15 @@
  * main - program that prints _putchar followed by a new line
  * Return: Always 0
  */
-int _putchar(void)
+int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar("u");
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char text[] = "_putchar";
+	int i;
+
+	for (i = 0 ; i < 8 ; i++)
+	{
+	_putchar(text[i]);
+	}
 	_putchar('\n');
 
 	return (0);
