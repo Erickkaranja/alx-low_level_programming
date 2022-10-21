2eb1e322c4d43b811bc4dadfb9d222372b8a9e5b
Erickkaranja <karanja99erick@gmail.com>
2022-10-21 13:17:48 -0400

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,17 +4,17 @@
  */
 void more_numbers(void)
 {
-	int i;
-	int n;
-	
+int i;
+int n;
+
 for (i = 0 ; i <= 14 ; i++)
-	{
-		for (n = 0 ; n <= 14 ; n++)
-		{
-			if (n > 9)
-		_putchar(1 + '0');
-		_putchar((i % 10) + '0');
-	}
-	_putchar('\n');
+{
+for (n = 0 ; n <= 14 ; n++)
+{
+if (n > 9)
+_putchar(1 + '0');
+_putchar((i % 10) + '0');
+}
+_putchar('\n');
 }
 }
