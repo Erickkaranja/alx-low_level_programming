4b42a12b29f72c6c0bbb142221155a7f3b39b565
Erickkaranja <karanja99erick@gmail.com>
2022-10-25 11:24:54 -0400

    print reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,9 +7,15 @@
 
 void print_rev(char *s)
 {
-while (*s)
+int length = 0;
+int index;
+
+while (s[index++])
+length++;
+
+for (index = length - 1 ; index >= 0 ; index--)
 {
-_putchar(*s--);
+_putchar(s[index]);
 }
 _putchar('\n');
 }
