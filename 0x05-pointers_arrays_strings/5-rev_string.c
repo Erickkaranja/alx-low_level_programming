2db6f800bcce2e3ed22431e4e8325846846a674a
Erickkaranja <karanja99erick@gmail.com>
2022-10-25 11:52:04 -0400

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,9 +7,12 @@
 
 void rev_string(char *s)
 {
-while (*s)
-{
-_putchar(*s--);
-}
+int length = 0;
+int index;
+while (s[index++])
+length++;
+
+for (index = length - 1 ; index >= 0 ; index++)
+_putchar(s[index]);
 _putchar('\n');
 }
