adf6767acb74017b229e630751584bb14994270b
Erickkaranja <karanja99erick@gmail.com>
2022-10-26 01:50:03 -0400

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,16 +12,10 @@
 char *_strcpy(char *dest, char *src)
 {
 int index = 0;
-int length = 0;
-
-while (*(src + length))
+while (src[index])
 {
-length++;
+dest[index] = src[index] + '\0';
+index++;
 }
-for (; index < length ; index++)
-{
-*(dest + 1) = *(src + 1);
-}
-*(dest + length) = '\0';
 return (dest);
 }
