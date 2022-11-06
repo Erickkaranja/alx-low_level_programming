caac27643a0bade58790b397bea1c8ce925463ab
Erickkaranja <karanja99erick@gmail.com>
2022-11-06 10:29:47 +0300

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+_memset() - copies bytes b pointed to from *s
+@s:Memory pointed to 
+@b:The character to be filled
+Return: the pointerto s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0 ; i < n ; i++)
+{
+*(s + i) = b;
+}
+return (s);
+}
