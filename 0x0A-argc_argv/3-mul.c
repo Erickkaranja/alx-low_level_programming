efbc0ec5eb9769a6d1a7a3bc665814aab0a96cad
Erickkaranja <karanja99erick@gmail.com>
2022-11-07 16:44:39 +0300

    add

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,11 +5,11 @@
 *main - function that multiplies two integers
 *@argc: The number arguement supplied to the program
 *@argv: Array of pointers to arguement
-
-Return: Always 0
+*
+*Return: Always 0
 */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 int num1, num2, prod;
 if (argc != 3)
@@ -19,8 +19,8 @@ return (1);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
+prod = (num1 * num2);
 
-prod = num1 * num2;
 printf("%d\n", prod);
 
 return (0);
