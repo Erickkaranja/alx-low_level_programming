617c414959ed70788699cac977340e8650aaafeb
Erickkaranja <karanja99erick@gmail.com>
2022-11-08 20:11:38 +0300

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+*create_array - 0creates an array of char and initializes
+*to a particular specific char
+@size: reserved bytes in memory
+@c: The value of character array
+Return: NULL if size=0 else pointer to c
+*/
+char *create_array(unsigned int size, char c)
+{
+char *array;
+unsigned int index;
+
+if (size == 0)
+{
+return (NULL);
+}
+if (array == NULL)
+{
+return (NULL);
+}
+for (index = 0 ; index < size ; index++)
+array[index] = c;
+return (array);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+*_strdup - returns a newly allocated space in memory
+*str: the string to be copied
+*Return: If str = NULL Return null
+*else pointer to the duplicate string
+*/
+
+char *_strdup(char *str)
+{
+char *s;
+int index, length = 0;
+
+if (str == NULL)
+{
+return (NULL);
+}
+for (index = 0 ; str[index] ; index++)
+length++;
+s = malloc (sizeof(char) * (length + 1));
+
+str[index] = s[index];
+
+return (s);
+
+}
