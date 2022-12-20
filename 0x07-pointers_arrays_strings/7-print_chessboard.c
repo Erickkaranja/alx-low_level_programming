6f26894e840315192eb3dbdb3e6e9351d754c181
Erickkaranja <karanja99erick@gmail.com>
2022-12-20 19:51:56 +0300

    completing task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_chessboard - prints a chess board.
+*
+*@a: the chess board columns.
+*/
+void print_chessboard(char (*a)[8])
+{
+unsigned int index1, index2;
+
+for (index1 = 0 ; a[index1][7] ; index1++)
+{
+	for (index2 = 0 ; index2 < 8 ; index2++)
+	{
+	_putchar(a[index1][index2]);
+	_putchar('\n');
+	}
+}
+}
