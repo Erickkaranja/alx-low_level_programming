ca4c386222b7f1d8b7118e12204113267adca2dc
Erickkaranja <karanja99erick@gmail.com>
2022-12-28 08:09:01 +0300

    struct

--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -3,12 +3,10 @@
 
 int main()
 {
-
 struct dog my_dog;
-my_dog.name = "tommy";
-my_dog.age = 2.00;
-my_dog.owner = "Karanja";
 
-printf("%s, %.2f, %s\n", my_dog.name, my_dog.age, my_dog.owner);
+init_dog(&my_dog, "Lexxi", 12.00, "Karanja");
+
+print_dog(&my_dog);
 return (0);
 }
