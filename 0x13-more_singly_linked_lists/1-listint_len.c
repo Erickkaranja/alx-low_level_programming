b3f03942676964944d621154c4aeefc04988c96f
Erickkaranja <karanja99erick@gmail.com>
2023-01-12 15:18:46 +0300

    list length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include "lists.h"
+
+/**
+*listint_len - prints the length of alinked list.
+*@h: pointer to the first element of the linked list.
+*
+*Return: The number of nodes.
+*
+*/
+
 size_t listint_len(const listint_t *h)
 {
 size_t nodes = 0;
