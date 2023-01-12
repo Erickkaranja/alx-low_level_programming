744c7e74788445cc902e506a47e5dd7549ad094f
Erickkaranja <karanja99erick@gmail.com>
2023-01-12 15:28:37 +0300

    sum linked list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
-*
-*
-*
+*sum_listint - adds the integer values in the linked list.
+*@head: pointer to the linked list.
+*Return: returns the sum.
 */
 int sum_listint(listint_t *head)
 {
@@ -11,7 +11,7 @@ int sum = 0;
 while (head)
 {
 sum += head->n;
-head = head->next; 
+head = head->next;
 }
 return (sum);
 }
