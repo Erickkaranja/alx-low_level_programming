982eadb25256ed389a64f8639ace83a248eb36a7
Erickkaranja <karanja99erick@gmail.com>
2023-02-06 06:27:13 +0300

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,6 +14,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table;
 	unsigned long int i;
 
+	if (size == NULL)
+		return (NULL);
+
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
@@ -25,5 +28,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	for (i = 0 ; i < size ; i++)
 		hash_table->array[i] = NULL;
-return (hash_table);
+
+	return (hash_table);
 }
