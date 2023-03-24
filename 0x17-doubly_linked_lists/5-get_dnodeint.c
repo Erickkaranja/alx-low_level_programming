84f6fe9567199d1b7eb8d026d6db371b890c4f69
Erickkaranja <karanja99erick@gmail.com>
2023-03-24 06:19:31 +0300

    correcting task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,23 @@
 #include "lists.h"
 /**
+* dlistint_len - finds of elements in a linked list.
+* @h: Pointer to the first element of the linked list.
+* Return: the number of elements in a linked list.
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
+
+/**
 * get_dnodeint_at_index - gets the node at a given index.
 * @head: The pointer to the initial node to the linked list.
 * @index: The index to be found.
@@ -9,13 +27,19 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-unsigned int node;
+unsigned int node, i;
+dlistint_t *nodeValue;
 
-for (node = 0 ; node < index ; node++)
+node = dlistint_len(head);
+
+if (index > node)
 {
-if (head == NULL)
 return (NULL);
-head = head->next;
 }
-return (head);
+nodeValue = head;
+for (i = 0 ; i < index ; i++)
+{
+nodeValue = nodeValue->next;
+}
+return (nodeValue);
 }
