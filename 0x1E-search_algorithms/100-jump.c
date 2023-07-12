4565c289acbf6fbeba2894d962a2528fb37d38b8
Erickkaranja <karanja99erick@gmail.com>
2023-07-12 07:07:06 +0300

    updating task 1

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include "search_algos.h"
+
+/**
+* jump_search - implementing jump search algorithm.
+* @array: The array of search.
+* @size: The size of the array.
+* @value: The value of search.
+* Return: index of the value in the array else -1.
+*/
+
+int jump_search(int *array, size_t s 
+	}
+}
