c916ac3b89c773de00953a5567313f1763e6e4e1
Erickkaranja <karanja99erick@gmail.com>
2023-09-11 22:25:26 +0300

    updating strstr function

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,16 +7,16 @@
 */
 char *_strpbrk(char *s, char *accept)
 {
-int index;
+	int index;
 
-while (*s)
-{
-for (index = 0 ; accept[index] ; index++)
-{
-if (*s == accept[index])
-return (s);
-}
-s++;
-}
-return ('\0');
+	while (*s)
+	{
+		for (index = 0 ; accept[index] ; index++)
+		{
+			if (*s == accept[index])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
 }
