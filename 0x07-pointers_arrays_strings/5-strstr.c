db9746b006462cd3b934274f745248aee9c42391
Erickkaranja <karanja99erick@gmail.com>
2023-09-11 22:31:13 +0300

    updating strstr doc

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,13 @@
 #include "main.h"
-/*
+
+/**
 *_strstr - checks for common substrings between two strings
 *@haystack: The string to be searched
 *@needle: The substring to be located
 *Return: if the substring is located a pointer to the beginning of
 *located substring.if substring no located return null.
 */
+
 char *_strstr(char *haystack, char *needle)
 {
 	int index;
@@ -20,7 +22,7 @@ char *_strstr(char *haystack, char *needle)
 		{
 			if (*needle == haystack[index])
 			{
-				return needle;
+				return (needle);
 			}
 		}
 	}
