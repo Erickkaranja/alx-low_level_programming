4b9cb39c5ba81d23281117e0ef858cf0d929f2de
Erickkaranja <karanja99erick@gmail.com>
2023-09-12 06:23:42 +0300

    updating print_chessboard function

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,14 +7,14 @@
 */
 void print_chessboard(char (*a)[8])
 {
-unsigned int index1, index2;
+	unsigned int index1, index2;
 
-for (index1 = 0 ; a[index1][7] ; index1++)
-{
-	for (index2 = 0 ; index2 < 8 ; index2++)
+	for (index1 = 0 ; a[index1][7] ; index1++)
 	{
-	_putchar(a[index1][index2]);
-	_putchar('\n');
+		for (index2 = 0 ; index2 < 8 ; index2++)
+		{
+			_putchar(a[index1][index2]);
+		}
+		_putchar('\n');
 	}
-}
 }
